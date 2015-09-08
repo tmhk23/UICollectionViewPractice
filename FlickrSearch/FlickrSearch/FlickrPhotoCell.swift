ca6abd390950f555f56c764c4641efa9aeab11b8
TomohikoHirose <hirose@agentec.jp>
2015-09-08 20:07:55 +0900

    part1終了

---
+++ FlickrSearch/FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Hirose on 2015/09/07.
+//  Copyright (c) 2015年 tmhk23. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
